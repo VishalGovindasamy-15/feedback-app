a4351af8259f53c1761bf623c35992c9af4534ee
VishalGovindasamy-15 <vishalgovindasamy@gmail.com>
2026-04-05 23:21:00 +0530

    test nagivation

--- feedback-app/app/app.py
+++ feedback-app/app/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    return "this is login page"
+    return render_template("login.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-if __name__=='__main__':
+if __name__ == "__main__":
     app.run(debug=True)
