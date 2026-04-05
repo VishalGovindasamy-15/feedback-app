f0bb8d125c24844b0a07830710c67168102b79c5
VishalGovindasamy-15 <vishalgovindasamy@gmail.com>
2026-04-05 22:53:26 +0530

    test the flask

--- feedback-app/app/app.py
+++ feedback-app/app/app.py
@@ -1,1 +1,15 @@
-from flask import flask 
+from flask import Flask 
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return "this is home page"
+
+@app.route('/login')
+def login():
+    return "this is login page"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
